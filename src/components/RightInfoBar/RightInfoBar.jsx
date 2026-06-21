074388019b3291b48d378893a562dcf5732da7e7
rafbradey <rafbradey.fm@gmail.com>
2026-06-22 02:53:46 +0800

    used navlinks so that current pages can be tracked, added a applayout for navbar support for multiple pages.

---
+++ src/components/RightInfoBar/RightInfoBar.jsx
@@ -0,0 +1,16 @@
+import "./RightInfoBar.css";
+
+function RightInfoBar(){
+    return(
+        <>
+            <div className="right-info-main">
+                <p>Streak: 2</p>
+                <p>Diamonds: 1235</p>
+                <p>Lives: 5</p>
+
+            </div>
+        </>
+    )
+}
+
+export default RightInfoBar;
--- src/pages/Learn/Learn.jsx
+++ src/pages/Learn/Learn.jsx
@@ -1,13 +1,23 @@
 import "./Learn.css";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import RightInfoBar from "../../components/RightInfoBar/RightInfoBar.jsx";
 
 
 function Learn(){
     return(
-        <>
+        <div className="page-wrapper">
+            <div className="app-layout">
             <Sidebar/>
+            </div>
 
-        </>
+
+            <div className="learn-page-content">
+                <p></p>
+            </div>
+            <div className="right-info-bar-learn">
+                <RightInfoBar/>
+            </div>
+        </div>
     )
 }
 
