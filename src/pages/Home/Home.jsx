cb33de58c0afebcf7086989710fda056c4d406a4
rafbradey <rafbradey.fm@gmail.com>
2026-06-21 16:02:23 +0800

    added mascot component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,22 +6,23 @@ function Home() {
 
         <main className="home-page">
             <section className="hero-section">
-                <div className="hero-content">
-                    <h1>Muchachos, Amigo Bueno.</h1>
 
-                    <p>
-                        Duoclongo Aribaaa!
-                    </p>
 
-                    <div className="hero-actions">
-                        <button className="primary-button">Get Started</button>
-                        <button className="secondary-button">I Already Have an Account</button>
-                    </div>
-                </div>
 
                 <div className="duoclongo-mascot">
-                    <Mascot mascotType="maracas" size={320} flipped={true} animationType="wiggle" />
+                    <Mascot mascotType="maracas" size={320} flipped={false} animationType="wiggle" />
                 </div>
+
+
+                <div className="hero-title">
+                    <h1>The free, fun, and effective way to learn about the types of drugs!</h1>
+                <div className="hero-actions">
+                    <button className="primary-button">Get Started</button>
+                    <button className="secondary-button">I Already Have an Account</button>
+                </div>
+                    </div>
+
+
 
             </section>
 
